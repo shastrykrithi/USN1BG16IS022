c968afb3132410a8d4eb92df72ad133c4cb6d610
krithi_shastry <shastrykrithi2197@gmail.com>
2019-02-23 06:33:48 +0530

    23-02-2019

---
+++ Samplewebsite/script.js
@@ -0,0 +1,14 @@
+//Challenge 1:
+
+var jm=50;
+var jh=170;
+var mm=60;
+var mh=165;
+var jbmi=jm/(jh*jh);
+var mbmi=mm/(mh*mh);
+if(jbmi>mbmi){
+    console.log('John BMI is greater'+jbmi);
+}
+else{
+    console.log('Mark BMI is greater'+mbmi);
+}
